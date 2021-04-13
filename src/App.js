40796ded30f637dc0e5623c6265de8851f50da64
raghav786 <58934695+raghav786@users.noreply.github.com>
2021-04-13 15:32:09 +0530

    commit gaaain

--- src/App.js
+++ src/App.js
@@ -16,4 +16,4 @@ function App() {
 export default App;
 
 
-// 02:50:00
+// 03:25:00
--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,5 @@
 import { Avatar } from '@material-ui/core';
-import React, { forwardRef } from 'react';
+import React, { useState, forwardRef } from 'react';
 import './Post.css';
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
@@ -15,6 +15,9 @@ const Post = forwardRef(({
     image,
     avatar
     }, ref) => {
+
+        const [ postLike, setPostLike ] = useState();
+
     return (
         <div  className='post' ref={ref}>
             <div className='post__avatar'>
@@ -46,10 +49,10 @@ const Post = forwardRef(({
                  />
 
                 <div className='post__footer'>
-                    <ChatBubbleOutlineIcon fontSize='small' />
-                    <RepeatIcon fontSize='small' />
-                    <FavoriteBorderIcon fontSize='small' />
-                    <PublishIcon fontSize='small' />
+                    <ChatBubbleOutlineIcon fontSize='small' className='postComment' />
+                    <RepeatIcon fontSize='small' className='postRetweet' />
+                    <FavoriteBorderIcon fontSize='small' className='postLike' />
+                    <PublishIcon fontSize='small' className='postBookMark' />
                 </div>
             
             </div>
